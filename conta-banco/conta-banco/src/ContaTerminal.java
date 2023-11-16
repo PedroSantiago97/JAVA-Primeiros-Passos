b3cf66a1d0afd3af7b9daf7acbcba55d01035dac
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-11-16 14:25:30 -0300

    ATTS

---
+++ conta-banco/conta-banco/src/ContaTerminal.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao Banco Dinheiro no bolso!");
+        System.out.printf("Digite sua conta: ");
+        int conta = scanner.nextInt();
+        
+        System.out.printf("Digite sua Agencia: ");
+        String agencia = scanner.next();
+        System.out.printf("Digite o Digito: ");
+        String digitoAgencia = scanner.next();
+
+        System.out.printf("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+        System.out.printf("Digite seu sobrenome: ");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.printf("Digite seu Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta no nosso banco, sua agência é "+ agencia+"-"+digitoAgencia + " conta " + conta + " e seu saldo que é de R$" + String.format("%.2f", saldo) + " ja está disponível !");
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- estudos-em-java/src/terminalTeste.java
+++ estudos-em-java/src/terminalTeste.java
@@ -1,5 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class terminalTeste {
     public static void main(String[] args) {
-        System.out.println("Consegui !");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite sua idade: ");
+        String idade = scanner.next();
+        System.out.println("Sua idade é: " + idade + " anos.");
+
+
     }
 }
