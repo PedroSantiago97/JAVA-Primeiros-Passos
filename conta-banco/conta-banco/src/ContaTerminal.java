e830cf4c56a5f11adfb468d59ed355642a96a699
PedroSantiago97 <pedro.santiago8051@outlook.com>
2023-11-16 15:45:37 -0300

    attt

--- conta-banco/conta-banco/src/ContaTerminal.java
+++ conta-banco/conta-banco/src/ContaTerminal.java
@@ -2,9 +2,14 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
+        try{
         System.out.println("Bem vindo ao Banco Dinheiro no bolso!");
         System.out.printf("Digite sua conta: ");
         int conta = scanner.nextInt();
@@ -23,15 +28,6 @@ public class ContaTerminal {
         double saldo = scanner.nextDouble();
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta no nosso banco, sua agência é "+ agencia+"-"+digitoAgencia + " conta " + conta + " e seu saldo que é de R$" + String.format("%.2f", saldo) + " ja está disponível !");
-
-
-
-
-
-
-
-
-
-
+        } finally{scanner.close();}
     }
 }
